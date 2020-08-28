e549f9c59c328db49dfdfcebd8436b734a1611bb
mayur.amritkar <mayur.amritkar@jetsynthesys.com>
2020-08-28 23:24:29 +0530

    Changing Margin

--- src/app/Author/ProfileCard/ProfileCard.component.ts
+++ src/app/Author/ProfileCard/ProfileCard.component.ts
@@ -10,8 +10,14 @@ import { Router, ActivatedRoute } from '@angular/router';
 export class ProfileCardComponent implements OnInit, DoCheck {
 
 
-  data = "In Informatics, dummy data is benign information that does not contain any useful data, but serves to reserve space where real data is nominally present. Dummy data can be used as a placeholder for both testing and operational purposes. For testing, dummy data can also be used as stubs or pad to avoid software testing issues by ensuring that all variables and data fields are occupied. In operational use, dummy data may be transmitted for OPSEC purposes. Dummy data must be rigorously evaluated and documented to ensure that it does not cause unintended effects.\
-  In Informatics, dummy data is benign information that does not contain any useful data, but serves to reserve space where real data is nominally present.Dummy data can be used as a placeholder for both testing and operational purposes.For testing, dummy data can also be used as stubs or pad to avoid software testing issues by ensuring that all variables and data fields are occupied.In operational use, dummy data may be transmitted for OPSEC purposes.Dummy data must be rigorously evaluated and documented to ensure that it does not cause unintended effects."
+  data = 'In Informatics, dummy data is benign information that does\
+   not contain any useful data,\
+   but serves to reserve space where real data is nominally present.\
+   Dummy data can be used as a placeholder for both testing and operational purposes.\
+    For testing, dummy data can also be used as stubs or pad to avoid software testing\
+    issues by ensuring that all variables and data fields are occupied. In operational use,\
+    dummy data may be transmitted for OPSEC purposes. Dummy data must be rigorously evaluated and documented\
+       to ensure that it does not cause unintended effects.';
 
 
   appUser: any;
